9717551086ac995ba31ffd545b675002e8d47384
hongquant17 <21020555@vnu.edu.vn>
2024-04-22 03:56:42 +0000

    add live capture

--- AppCheckin/AppCheckin/CheckinPage.xaml.cs
+++ AppCheckin/AppCheckin/CheckinPage.xaml.cs
@@ -12,10 +12,13 @@ namespace AppCheckin
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class CheckinPage : ContentPage
 	{
+        bool PermissionsGranted;
+        private int frameCounter = 0; 
+
 		public CheckinPage ()
 		{
 			InitializeComponent ();
-    
+            cameraView.CameraOptions.AutoRotateImage = true;
         }
 
         void OnCaptureClicked(object sender, EventArgs e)
@@ -35,9 +38,42 @@ namespace AppCheckin
             captureButton.IsEnabled = e;
         }
 
+        // private async void MainPage_Appearing(object sender, EventArgs e) {
+        //     PermissionsGranted = await VerifyPermissions();
+        //     if (PermissionsGranted == false) 
+        //         return;
+
+        //     else {
+        //         App.Current.MainPage
+        //     }
+        // }
+
+        private async Task<bool> VerifyPermissions() {
+            try {
+                PermissionStatus status = PermissionStatus.Unknown;
+
+                status = await Permissions.CheckStatusAsync<Permissions.Camera>();
+
+                if (status != PermissionStatus.Granted) {
+                    await DisplayAlert("Camera Permission Required", "This app will proccess frames taken live from the device's camera", "OK");
+                    status = await Permissions.RequestAsync<Permissions.Camera>(); 
+
+                    if (status != PermissionStatus.Granted) {
+                        return false;
+                    }
+                }
+
+                return true;
+            } catch(Exception ex) {
+                await DisplayAlert("Error", ex.ToString(), "OK");
+                return false;
+            }
+
+        }
+
         void CameraView_MediaCapture(object sender, MediaCapturedEventArgs e)
         {
-            previewPicture.IsVisible = true;
+            // previewPicture.IsVisible = true;
             previewPicture.Rotation = e.Rotation;
             previewPicture.Source = e.Image;
             cameraView.IsVisible = false;
@@ -57,7 +93,23 @@ namespace AppCheckin
             Console.WriteLine(DeviceDisplay.MainDisplayInfo.Height);
             Console.WriteLine(DeviceDisplay.MainDisplayInfo.Width);
             Console.WriteLine("____________________");
+            
+
+            if (captureButton.Text == "Live Capture") {
+                captureButton.Text = "Stop";
+                cameraView.FrameReceived += Camera_FrameReceived;
+            }
+            else {
+                captureButton.Text = "Live Capture";
+                cameraView.FrameReceived -= Camera_FrameReceived;
+            }
         }
 
+        private void Camera_FrameReceived(Leadtools.Camera.Xamarin.FrameHandlerEventArgs e) {
+            frameCounter++;
+            Device.BeginInvokeOnMainThread(() => {
+                alignFaceText.Text = $"Frames Processed: {frameCounter}"; 
+            });
+        }
     }
 }
